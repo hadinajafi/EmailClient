35937cc59320743bab24646c9b9d800af49614f7
hadinajfi77 <hadinajafi.77@gmail.com>
2018-12-13 14:20:08 +0330

    add icon resolver to the treeView

--- EmailClient/src/emailclient/MainLayoutController.java
+++ EmailClient/src/emailclient/MainLayoutController.java
@@ -12,10 +12,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.web.WebView;
 
 /**
@@ -28,12 +32,12 @@ public class MainLayoutController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
     @FXML
     private WebView emailWebView;
 
     @FXML
-    private TreeView<?> treeView;
+    private TreeView<String> treeView;
+    private TreeItem<String> root = new TreeItem<>();
 
     @FXML
     private TableView<EmailMessageBean> tableView;
@@ -47,13 +51,65 @@ public class MainLayoutController implements Initializable {
     @FXML
     private TableColumn<EmailMessageBean, String> sizeColumn;
 
+    private void addCategoriesTreeItems() {
+        TreeItem<String> inbox = new TreeItem<>("Inbox", resolveIcon("Inbox"));
+        TreeItem<String> sent = new TreeItem<>("Sent", resolveIcon("Sent"));
+        TreeItem<String> spam = new TreeItem<>("Spam", resolveIcon("Spam"));
+        TreeItem<String> trash = new TreeItem<>("Trash", resolveIcon("Trash"));
+        root.getChildren().addAll(inbox, sent, spam, trash);
+        ImageView rootAvatar = new ImageView(new Image("/icons/avatar.png"));
+        rootAvatar.setFitWidth(24);
+        rootAvatar.setFitHeight(24);
+        root.setGraphic(rootAvatar);
+        root.setExpanded(true);
+    }
+
+    //resolve icons for treeView nodes:
+    private Node resolveIcon(String treeItemValue) {
+        String lowercaseTreeItemValue = treeItemValue.toLowerCase();
+        ImageView returnImage;
+        try {
+            if (lowercaseTreeItemValue.contains("inbox")) {
+                returnImage = new ImageView(new Image(getClass().getResourceAsStream("/icons/inbox.png")));
+                returnImage.setFitHeight(24);
+                returnImage.setFitWidth(24);
+            } else if (lowercaseTreeItemValue.contains("sent")) {
+                returnImage = new ImageView(new Image(getClass().getResourceAsStream("/icons/chat.png")));
+                returnImage.setFitWidth(24);
+                returnImage.setFitHeight(24);
+            } else if (lowercaseTreeItemValue.contains("spam")) {
+                returnImage = new ImageView(new Image(getClass().getResourceAsStream("/icons/spam.png")));
+                returnImage.setFitWidth(24);
+                returnImage.setFitHeight(24);
+            } else if (lowercaseTreeItemValue.contains("@")) {
+                returnImage = new ImageView(new Image(getClass().getResourceAsStream("/icons/email.png")));
+                returnImage.setFitWidth(24);
+                returnImage.setFitHeight(24);
+            } 
+            else if(lowercaseTreeItemValue.contains("trash")){
+                returnImage = new ImageView(new Image(getClass().getResourceAsStream("/icons/bin.png")));
+                returnImage.setFitWidth(24);
+                returnImage.setFitHeight(24);
+            }
+            else {
+                returnImage = new ImageView(new Image(getClass().getResourceAsStream("/icons/folder.png")));
+                returnImage.setFitWidth(24);
+                returnImage.setFitHeight(24);
+            }
+            return returnImage;
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
     /*
     private ObservableList<EmailMessageBean> data = FXCollections.observableArrayList(
             new EmailMessageBean("Hello Subject1", "hello@ymail.com", 450000),
             new EmailMessageBean("hello2", "hello@gmail.ir", 34),
             new EmailMessageBean("hadi", "test@yahoo.com", 3456)
     );*/
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         subjectColumn.setCellValueFactory(new PropertyValueFactory<>("subject"));
@@ -66,6 +122,11 @@ public class MainLayoutController implements Initializable {
             Integer int2 = EmailMessageBean.formattedValues.get(o2);
             return int1.compareTo(int2);
         });
-    }    
-    
+
+        //set root item as root in tree view:
+        treeView.setRoot(root);
+        root.setValue("example@gmail.com");
+        addCategoriesTreeItems();
+    }
+
 }
