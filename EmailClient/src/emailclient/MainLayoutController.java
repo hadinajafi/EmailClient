8cc1532aa848bbfec7db772aa278c5b0e786e496
hadinajfi77 <hadinajafi.77@gmail.com>
2018-12-13 11:38:55 +0330

    override compare method for size column

--- EmailClient/src/emailclient/MainLayoutController.java
+++ EmailClient/src/emailclient/MainLayoutController.java
@@ -6,7 +6,9 @@
 package emailclient;
 
 import java.net.URL;
+import java.util.Comparator;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -43,15 +45,27 @@ public class MainLayoutController implements Initializable {
     private TableColumn<EmailMessageBean, String> senderColumn;
 
     @FXML
-    private TableColumn<EmailMessageBean, Integer> sizeColumn;
+    private TableColumn<EmailMessageBean, String> sizeColumn;
 
-    private ObservableList<EmailMessageBean> data;
+    /*
+    private ObservableList<EmailMessageBean> data = FXCollections.observableArrayList(
+            new EmailMessageBean("Hello Subject1", "hello@ymail.com", 450000),
+            new EmailMessageBean("hello2", "hello@gmail.ir", 34),
+            new EmailMessageBean("hadi", "test@yahoo.com", 3456)
+    );*/
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         subjectColumn.setCellValueFactory(new PropertyValueFactory<>("subject"));
         senderColumn.setCellValueFactory(new PropertyValueFactory<>("sender"));
         sizeColumn.setCellValueFactory(new PropertyValueFactory<>("size"));
+        //tableView.setItems(data);
+        //override the comperator method for size column:
+        sizeColumn.setComparator((String o1, String o2) -> {
+            Integer int1 = EmailMessageBean.formattedValues.get(o1);
+            Integer int2 = EmailMessageBean.formattedValues.get(o2);
+            return int1.compareTo(int2);
+        });
     }    
     
 }
